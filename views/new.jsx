a481deee4bf0080ba0301a6c2b400a8679496202
Ashley Vieira <ash.adr24212@gmail.com>
2022-10-26 19:17:07 -0400

    new recipe form css

--- views/new.jsx
+++ views/new.jsx
@@ -10,18 +10,26 @@ function New ({recipes}) {
                 <a href = "/bakery"><button>Go back to the index</button></a>
             </div> */}
             <form action = "/bakery" method = "POST">
-                <label htmlFor = "name">Recipe Name</label>
+                <label htmlFor = "name">Recipe Name<a style={{ marginLeft: '2rem' , lineHeight: '100px'}}></a></label>
                 <input
                     type = "text"
                     name = "name"
                     id = "name"
                     required
                 />
-                <label htmlFor = "image">Image URL</label>
+                <br />
+                <label htmlFor = "image">Image URL<a style={{ marginLeft: '3rem' , lineHeight: '100px' }}></a></label>
                 <input
                     type = "text"
                     name = "image"
                     id = "image"
+                />
+                <br />
+                <label htmlFor = "comments">Comments<a style={{ marginLeft: '3rem' , lineHeight: '100px' }}></a></label>
+                <input
+                    type = "text"
+                    name = "text"
+                    id = "text"
                 />
                 <br />
                 <input type = "submit" />
